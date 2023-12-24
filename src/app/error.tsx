132a9f814af8a3705697ef91a9702caabf44bb01
shaik-zeeshan <shaikzeeshan999@gmail.com>
2023-12-24 13:11:27 +0530

    Change Error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 export default function Error({
-                                  error,
-                                  reset,
-                              }: {
-    error: Error & { digest?: string }
-    reset: () => void
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
 }) {
-    return <div className="flex flex-col min-h-screen w-full items-center justify-center ">
-      <div className="space-y-5 text-center">
-          <div className="text-2xl">{error.message}</div>
-          <button onClick={reset} className="bg-black px-4 py-2 rounded-lg text-gray-100">Reset</button>
-      </div>
-    </div>
+    return (
+        <div className="flex min-h-screen w-full flex-col items-center justify-center ">
+            <div className="space-y-5 text-center">
+                <div className="text-2xl">{error.message}</div>
+                <button
+                    onClick={reset}
+                    className="rounded-lg bg-black px-4 py-2 text-gray-100"
+                >
+                    Reset
+                </button>
+            </div>
+        </div>
+    );
 }
